c18ecb2460c93c572383ee8447af1adf39e57a8d
Harrisonfishco <harrisonfishco@gmail.com>
2023-01-04 22:33:18 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace pdfw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mergeButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PDF Merger";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(12, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mergeButton
+            // 
+            this.mergeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.mergeButton.AutoSize = true;
+            this.mergeButton.Enabled = false;
+            this.mergeButton.Location = new System.Drawing.Point(713, 408);
+            this.mergeButton.Name = "mergeButton";
+            this.mergeButton.Size = new System.Drawing.Size(75, 30);
+            this.mergeButton.TabIndex = 2;
+            this.mergeButton.Text = "Merge";
+            this.mergeButton.UseVisualStyleBackColor = true;
+            this.mergeButton.Click += new System.EventHandler(this.mergeButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.AllowDrop = true;
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.AutoArrange = false;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(10, 67);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(778, 325);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDown);
+            this.listView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseMove);
+            this.listView1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseUp);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "msedge_IDR_X009_PDF_ICON.ico");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.mergeButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(822, 506);
+            this.MinimumSize = new System.Drawing.Size(822, 506);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button mergeButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp.Pdf;
+
+namespace pdfw
+{
+    public partial class Form1 : Form
+    {
+
+        private List<string> filenames;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "PDF Merger";
+            filenames = new List<string>();
+
+            typeof(Control).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(listView1, true, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Test");
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.FileOk += fileDialogOk;
+            fileDialog.Filter = "Portable Document Format|*.pdf";
+            fileDialog.Multiselect = true;
+            fileDialog.ShowDialog();
+        }
+
+        private void fileDialogOk(object sender, CancelEventArgs e)
+        {
+            if(!e.Cancel)
+            {
+                string[] files = ((OpenFileDialog)sender).FileNames;
+
+                foreach (string file in files)
+                {
+                    filenames.Add(file);
+                    ListViewItem item = new ListViewItem(new FileInfo(file).Name);
+                    item.ToolTipText = file;
+                    item.ImageIndex = 0;
+                    listView1.Items.Add(item);
+                }
+
+                if(filenames.Count > 1)
+                    mergeButton.Enabled = true;
+            }
+        }
+
+        private string[] getItemsInOrder()
+        {
+            string[] ordered = new string[listView1.Items.Count];
+            SortedDictionary<Tuple<int, int>, string> points = new SortedDictionary<Tuple<int, int>, string>();
+            foreach(ListViewItem item in listView1.Items)
+            {
+                Tuple<int, int> tp = new Tuple<int, int>(item.Position.Y, item.Position.X);
+                points.Add(tp, item.ToolTipText);
+            }
+
+            int i = 0;
+            foreach(KeyValuePair<Tuple<int, int>, string> kvp in points)
+            {
+                ordered[i] = kvp.Value;
+                ++i;
+            }
+
+            return ordered;
+        }
+
+        private void mergeButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileOk += saveFileOk;
+            saveFileDialog.Filter = "Portal Document Format|*.pdf";
+            saveFileDialog.ShowDialog();
+        }
+
+        private void saveFileOk(object sender, CancelEventArgs e)
+        {
+            PdfDocument[] docs = new PdfDocument[filenames.Count];
+            int i = 0;
+
+            PdfDocument doc = new PdfDocument();
+
+            foreach (string filename in getItemsInOrder())
+            {
+                docs[i] = PdfSharp.Pdf.IO.PdfReader.Open(filename, PdfSharp.Pdf.IO.PdfDocumentOpenMode.Import);
+                foreach (PdfPage page in docs[i].Pages)
+                {
+                    doc.AddPage(page);
+                }
+                ++i;
+            }
+
+            doc.Save(((SaveFileDialog)sender).FileName);
+
+            filenames.Clear();
+            listView1.Clear();
+            mergeButton.Enabled = false;
+        }
+
+        ListViewItem heldDownItem;
+        Point heldDownPoint;
+
+        private void listView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            listView1.AutoArrange = false;
+            heldDownItem = listView1.GetItemAt(e.X, e.Y);
+            if(heldDownItem != null)
+            {
+                heldDownPoint = new Point(e.X - heldDownItem.Position.X, e.Y - heldDownItem.Position.Y);
+            }
+        }
+
+        private void listView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(heldDownItem != null)
+            {
+                heldDownItem.Position = new Point(e.Location.X - heldDownPoint.X, e.Location.Y - heldDownPoint.Y);
+            }
+        }
+
+        private void listView1_MouseUp(object sender, MouseEventArgs e)
+        {
+            heldDownItem = null;
+            listView1.AutoArrange = true;
+        }
+    }
+}
